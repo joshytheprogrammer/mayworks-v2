15df619524a64d11713621ae1aeff91660866dcc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 00:10:01 +0100

    made changes

--- app.vue
+++ app.vue
@@ -6,3 +6,14 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+import { useMobileStore } from '@/store/mobile'
+
+const device = useMobileStore()
+
+// Call the checkWidth ACTION
+device.checkWidth();
+
+let isMobile = ref(device.getDevice);
+</script>
