6378e0649e19553cd27fbd5bb44f7ff1a321bc0f
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 00:40:22 +0100

    made changes

--- components/App/Nav/Strip.vue
+++ components/App/Nav/Strip.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="w-full px-4 sm:px-8 md:px-12 lg:px-28 bg-purple-900 text-white flex items-center justify-between">
-
+  <div class="w-full px-4 py-4 sm:px-8 md:px-12 lg:px-28 bg-blue-900 text-white flex items-center justify-between">
+    <div class="text-sm flex items-center">
+      <Icon name="material-symbols:mail" size="1.25em" />
+      <span class="pl-1">
+        Email: site@mayworkslimited.com
+      </span>
+    </div>
+    <div class="text-sm flex items-center">
+      <Icon name="material-symbols:location-on" size="1.25em" />
+      <span class="pl-1">
+        1 Joseph Street, Lagos Island, Lagos, Nigeria
+      </span>
+    </div>
   </div>
 </template>
