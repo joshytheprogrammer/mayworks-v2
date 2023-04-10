897b4d5bc996362654386399ae6d79e67866a2d2
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 20:52:48 +0100

    made changes

--- components/Home/Testimonials.vue
+++ components/Home/Testimonials.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  bg-repeat bg-blue-100">
+  <div class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32 bg-repeat bg-blue-100">
     <h2 id="testimonials" class="font-medium text-blue-900 text-center">TESTIMONIALS</h2>
     <h2 class="py-12 lg:w-1/2 m-auto text-4xl leading-snug font-semibold text-center">What Clients Says <span class="bg-gradient-to-b from-blue-500 to-blue-900 text-transparent bg-clip-text"> <br> About Us</span></h2>
     <div class="flex justify-between text-neutral-100">
