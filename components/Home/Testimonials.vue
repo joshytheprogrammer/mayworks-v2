ff8ffd9f187b73efee3f232244e67ac42a7080f2
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 18:00:01 +0100

    made changes

--- components/Home/Testimonials.vue
+++ components/Home/Testimonials.vue
@@ -2,5 +2,14 @@
   <div id="testimonials" class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  bg-repeat bg-blue-100">
     <h2 class="font-medium text-blue-900 text-center">TESTIMONIALS</h2>
     <h2 class="py-12 lg:w-1/2 m-auto text-4xl leading-snug font-semibold text-center">What Clients Says <span class="bg-gradient-to-b from-blue-500 to-blue-900 text-transparent bg-clip-text"> <br> About Us</span></h2>
+    <div class="flex justify-between text-neutral-100">
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-600 opacity-70">
+        <p class="font-medium text-base pb-4">Working with Mayworks has been a game changer for our business. They helped us navigate complex regulations and logistics to successfully export our products overseas. Their team is knowledgeable, reliable, and always available to provide support. I highly recommend Mayworks to any company looking to expand their reach and solve complex problems.</p>
+        <div class="">
+          <p class="font-semibold">Emeka Asoha</p>
+          <p class="text-sm">Contractor</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
