aa8d1a9d8f80681f523e0debd19166372b70b727
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 20:45:50 +0100

    made changes

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="text-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32 flex justify-between bg-repeat h-fit md:h-[520px] bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/v1681085602/mayworks/jumbotron_nk3w3q.jpg')]">
+  <div class="text-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32 flex justify-between bg-repeat h-fit bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/v1681144237/mayworks/bg-2_tclytz.jpg')]">
     <div class="flex flex-col justify-center">
       <div class="pb-4">
         <h1 class="text-4xl lg:text-6xl leading-snug font-bold md:w-full ">We Get Business Deals Done</h1>
-        <!-- <h4 class="text-xl lg:text-4xl lg:w-1/2 font-bold pt-2"></h4> -->
       </div>
       <p class="py-4 leading-7 lg:pb-12 md:w-full lg:w-1/2 text-neutral-200">The trusted Mayworks is an exporter of minerals from Nigeria to the west, with a proven track record. In addition, the company offers fast and reliable solutions to assist businesses in problem-solving, dedicated to maintaining its reputation as a reliable partner.</p>
       <div class="flex pt-4 sm:pt-0">
--- components/Home/Testimonials.vue
+++ components/Home/Testimonials.vue
@@ -3,21 +3,21 @@
     <h2 id="testimonials" class="font-medium text-blue-900 text-center">TESTIMONIALS</h2>
     <h2 class="py-12 lg:w-1/2 m-auto text-4xl leading-snug font-semibold text-center">What Clients Says <span class="bg-gradient-to-b from-blue-500 to-blue-900 text-transparent bg-clip-text"> <br> About Us</span></h2>
     <div class="flex justify-between text-neutral-100">
-      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-600 opacity-70 hover:opacity-100">
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-800 opacity-70 hover:opacity-100">
         <p class="font-medium text-base pb-4">Working with Mayworks has been a game changer for our business. Their expertise and dedication to providing solutions that work have helped us overcome challenges we thought were impossible to tackle. I highly recommend them to any business in need of a reliable partner.</p>
         <div class="">
           <p class="font-semibold">Funke Adekunle,</p>
           <p class="text-sm">CEO of Adekunle Enterprises</p>
         </div>
       </div>
-      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-600">
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-800">
         <p class="font-medium text-base pb-4">Mayworks has proven to be a valuable asset to our company. Their knowledge of the industry and commitment to delivering high-quality services have enabled us to achieve our goals faster than we thought possible. We are grateful for their partnership and look forward to working with them for years to come.</p>
         <div class="">
           <p class="font-semibold">Tunde Bakare,</p>
           <p class="text-sm">Director of Operations at Bakare & Sons Ltd.</p>
         </div>
       </div>
-      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-600 opacity-70 hover:opacity-100">
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-800 opacity-70 hover:opacity-100">
         <p class="font-medium text-base pb-4">I was impressed by the professionalism and efficiency of Mayworks. They helped our company solve a critical problem that was affecting our business operations, and they did it in record time. The team was knowledgeable, reliable, and always available to answer our questions. I would highly recommend Mayworks to any organization in need of fast and effective solutions.</p>
         <div class="">
           <p class="font-semibold">Chiamaka Okonkwo,</p>
