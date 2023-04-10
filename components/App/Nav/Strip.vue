c33396f38126c76faa8d27a170874dfabe1fa9dc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 01:33:48 +0100

    made changes

--- components/App/Nav/Strip.vue
+++ components/App/Nav/Strip.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full px-4 py-4 sm:px-8 md:px-12 lg:px-28 bg-blue-900 text-white flex items-center justify-between">
+  <div class="w-full px-4 py-4 sm:px-8 md:px-12 lg:px-28 bg-gradient-to-l from-blue-700 to-blue-900 text-white flex items-center justify-between">
     <div class="text-sm flex items-center">
       <Icon name="material-symbols:mail" size="1.25em" />
       <span class="pl-1">
