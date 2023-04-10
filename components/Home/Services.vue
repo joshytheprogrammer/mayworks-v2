3cac744096cbf037541fb5cf5b5e36203c17aee3
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 17:19:21 +0100

    made changes

--- components/Home/Services.vue
+++ components/Home/Services.vue
@@ -1,9 +1,58 @@
 <template>
   <div id="services" class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  bg-blue-950 text-white">
-    <h2 class="pb-4 font-medium text-blue-400 text-center">SERVICES</h2>
-    <h2 class="lg:w-1/2 m-auto text-4xl leading-snug font-semibold text-center">Our awesome services for <span class="bg-gradient-to-b from-blue-300 to-blue-500 text-transparent bg-clip-text"> valued customers</span></h2>
-    <div class="">
-      
+    <h2 class="font-medium text-blue-400 text-center">SERVICES</h2>
+    <h2 class="py-12 lg:w-1/2 m-auto text-4xl leading-snug font-semibold text-center">Our awesome services for <span class="bg-gradient-to-b from-blue-300 to-blue-500 text-transparent bg-clip-text"> valued customers</span></h2>
+    <div class="flex justify-between flex-wrap text-center text-white ">
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-l from-blue-500 to-blue-700">
+        <figure class="text-white">
+          <Icon name="tabler:ship" size="4.5em" />
+        </figure>
+        <div class="">
+          <h2 class="text-2xl font-semibold">Exporting minerals</h2>
+          <p class="py-4 text-sm leading-7">Mayworks provides services for exporting minerals, including Zinc, Lead, and Charcoal, from Nigeria to various countries in the west, such as the US, UK, and China.</p>
+          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+        </div>
+      </div>
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-blue-500 to-blue-700">
+        <figure class="text-white">
+          <Icon name="clarity:contract-line" size="4.5em" />
+        </figure>
+        <div class="">
+          <h2 class="text-2xl font-semibold">Contract execution</h2>
+          <p class="py-4 text-sm leading-7">Mayworks assists companies with drafting, negotiating, executing, and managing contracts, helping to ensure that businesses can operate smoothly and minimize legal risks.</p>
+          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+        </div>
+      </div>
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-l from-blue-500 to-blue-700">
+        <figure class="text-white">
+          <Icon name="material-symbols:problem-outline" size="4.5em" />
+        </figure>
+        <div class="">
+          <h2 class="text-2xl font-semibold">Problem Solving</h2>
+          <p class="py-4 text-sm leading-7">Mayworks provides consulting services to help businesses and governments solve problems quickly and efficiently. This could involve conducting research, analyzing data, and developing strategies to achieve specific goals.</p>
+          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+        </div>
+      </div>
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-blue-500 to-blue-700">
+        <figure class="text-white">
+          <Icon name="material-symbols:manage-accounts" size="4.5em" />
+        </figure>
+        <div class="">
+          <h2 class="text-2xl font-semibold">Logistics</h2>
+          <p class="py-4 text-sm leading-7">Mayworks could provide services related to logistics and supply chain management, helping businesses to manage their inventory, shipping, and delivery processes more efficiently and effectively.</p>
+          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+        </div>
+      </div>
+      <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-l from-blue-500 to-blue-700">
+        <figure class="text-white">
+          <Icon name="material-symbols:partner-exchange" size="4.5em" />
+        </figure>
+        <div class="">
+          <h2 class="text-2xl font-semibold">Partnerships</h2>
+          <p class="py-4 text-sm leading-7">Mayworks helps businesses form partnerships and collaborations with other companies and organizations, helping to create new opportunities for growth and development.</p>
+          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
