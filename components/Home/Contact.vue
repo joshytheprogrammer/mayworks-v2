e8172cf0dc159ccf58494121fb67c414aae9aa9d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 22:56:15 +0100

    made changes

--- components/Home/Contact.vue
+++ components/Home/Contact.vue
@@ -47,7 +47,7 @@
         <input class="bg-blue-500 w-full px-4 my-4 h-12 placeholder:text-slate-200 outline-0" type="text" placeholder="Email">
         <input class="bg-blue-500 w-full px-4 h-12 placeholder:text-slate-200 outline-0" type="text" placeholder="Phone">
         <textarea class="bg-blue-500 w-full px-4 py-4 my-4 h-36 placeholder:text-slate-200 outline-0" type="text" placeholder="Write your message here..."></textarea>
-        <button class="px-8 py-4 bg-gradient-to-l hover:bg-gradient-to-r from-blue-500 to-blue-700">Submit Now</button>
+        <button class="px-8 py-4 bg-blue-700">Submit Now</button>
       </form>
     </div>
   </div>
