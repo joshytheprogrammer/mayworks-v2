c77222fb89f193531be04c48e9d091a8f798335d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-10 20:32:21 +0100

    made changes

--- components/Home/Services.vue
+++ components/Home/Services.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="services" class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  bg-blue-950 text-white">
-    <h2 class="font-medium text-blue-400 text-center">SERVICES</h2>
+  <div class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  bg-blue-950 text-white">
+    <h2 id="services" class="font-medium text-blue-400 text-center">SERVICES</h2>
     <h2 class="py-12 lg:w-1/2 m-auto text-4xl leading-snug font-semibold text-center">Our awesome services for <span class="bg-gradient-to-b from-blue-300 to-blue-500 text-transparent bg-clip-text"> valued customers</span></h2>
     <div class="flex justify-between flex-wrap text-center text-white ">
       <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-l from-blue-500 to-blue-700">
--- components/Home/Testimonials.vue
+++ components/Home/Testimonials.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="testimonials" class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  bg-repeat bg-blue-100">
-    <h2 class="font-medium text-blue-900 text-center">TESTIMONIALS</h2>
+  <div class="py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  bg-repeat bg-blue-100">
+    <h2 id="testimonials" class="font-medium text-blue-900 text-center">TESTIMONIALS</h2>
     <h2 class="py-12 lg:w-1/2 m-auto text-4xl leading-snug font-semibold text-center">What Clients Says <span class="bg-gradient-to-b from-blue-500 to-blue-900 text-transparent bg-clip-text"> <br> About Us</span></h2>
     <div class="flex justify-between text-neutral-100">
       <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 bg-blue-600 opacity-70 hover:opacity-100">
