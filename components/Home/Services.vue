35fd8d245a9e5dd69c56e4db783658028f2fc06c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-12 05:10:15 +0100

    made changes

--- components/Home/Services.vue
+++ components/Home/Services.vue
@@ -10,7 +10,7 @@
         <div class="">
           <h2 class="text-2xl font-semibold">Exporting minerals</h2>
           <p class="py-4 text-sm leading-7">Mayworks provides services for exporting minerals, including Zinc, Lead, and Charcoal, from Nigeria to various countries in the west, such as the US, UK, and China.</p>
-          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+          <a class="font-medium" href="https://wa.me/2348088985577">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
         </div>
       </div>
       <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-blue-500 to-blue-700">
@@ -20,7 +20,7 @@
         <div class="">
           <h2 class="text-2xl font-semibold">Contract execution</h2>
           <p class="py-4 text-sm leading-7">Mayworks assists companies with drafting, negotiating, executing, and managing contracts, helping to ensure that businesses can operate smoothly and minimize legal risks.</p>
-          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+          <a class="font-medium" href="https://wa.me/2348088985577">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
         </div>
       </div>
       <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-l from-blue-500 to-blue-700">
@@ -30,7 +30,7 @@
         <div class="">
           <h2 class="text-2xl font-semibold">Problem Solving</h2>
           <p class="py-4 text-sm leading-7">Mayworks provides consulting services to help businesses and governments solve problems quickly and efficiently. This could involve conducting research, analyzing data, and developing strategies to achieve specific goals.</p>
-          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+          <a class="font-medium" href="https://wa.me/2348088985577">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
         </div>
       </div>
       <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-blue-500 to-blue-700">
@@ -40,7 +40,7 @@
         <div class="">
           <h2 class="text-2xl font-semibold">Logistics</h2>
           <p class="py-4 text-sm leading-7">Mayworks could provide services related to logistics and supply chain management, helping businesses to manage their inventory, shipping, and delivery processes more efficiently and effectively.</p>
-          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+          <a class="font-medium" href="https://wa.me/2348088985577">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
         </div>
       </div>
       <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-l from-blue-500 to-blue-700">
@@ -50,7 +50,7 @@
         <div class="">
           <h2 class="text-2xl font-semibold">Partnerships</h2>
           <p class="py-4 text-sm leading-7">Mayworks helps businesses form partnerships and collaborations with other companies and organizations, helping to create new opportunities for growth and development.</p>
-          <a class="font-medium" href="">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
+          <a class="font-medium" href="https://wa.me/2348088985577">Learn more <Icon name="material-symbols:arrow-right-alt" /> </a>
         </div>
       </div>
     </div>
