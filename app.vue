f1ef2bed2feb6357cf1e4ef9951748589fb64198
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-13 04:51:19 +0100

    made changes

--- app.vue
+++ app.vue
@@ -19,4 +19,11 @@ const device = useMobileStore()
 device.checkWidth();
 
 let isMobile = ref(device.getDevice);
+
+useSeoMeta({
+  title: 'Welcome to the official website for Mayworks Limited',
+  meta: [
+    { name: 'description', content: 'Mayworks is a trusted exporter of minerals from Nigeria to several countries in the west, with an outstanding track record in the industry. Additionally, the company assists both small and large businesses in problem-solving, offering fast and reliable solutions to help organizations achieve their goals. Mayworks is dedicated to maintaining its reputation as a trustworthy and reliable partner to all its clients.' }
+  ]
+});
 </script>
